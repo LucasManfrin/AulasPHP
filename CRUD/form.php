a54c600320343b7240af6411baf300849560584b
LUCAS MANFRIN BATISTA <lucas.mbatista7@senacsp.edu.br>
2024-09-03 12:21:48 -0300

    att

--- CRUD/form.php
+++ CRUD/form.php
@@ -7,11 +7,11 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-light">
     <nav class="navbar bg-body-tertiary">
     <div class="container-fluid">
          <a class="navbar-brand" href="#">
-            <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">Bootstrap
+            <img src="./Imagens/cadastro.png" alt="Logo" width="39" height="39" class="d-inline-block align-text-top">
         </a>
     </div>
     </nav >
@@ -89,7 +89,7 @@
                 </form>
         </div>
 <footer>
-    <nav class="navbar">
+    <nav class="navbar fixed-bottom">
         <div>
             Todos os direitos reservados - 2024
         </div>
